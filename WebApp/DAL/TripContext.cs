4e728b9acbdc97bcc5802f6f433b2efb2a45c85c
Lindca <lindsaycampbell1994@gmail.com>
2020-02-14 00:42:16 -0800

    Adding Db Context

---
+++ WebApp/DAL/TripContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApp.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace WebApp.DAL
+{
+    public class TripContext: DbContext
+    {
+        public TripContext() : base("TripContext")
+        {
+        }
+
+        public DbSet<TripModel> Trips { get; set; }
+        public DbSet<EventModel> Events { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
